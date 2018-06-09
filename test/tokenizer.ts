45755a70c7d2816782c813a2b6d82057bb919402
Dong Yeop Lee <dongy7@gmail.com>
2018-06-09 14:56:47 -0400

    add jest

---
+++ test/tokenizer.ts
@@ -0,0 +1,17 @@
+import Compiler from '../src/compiler'
+
+it('tokenizes number', () => {
+  expect(Compiler.tokenizer('123')).toMatchSnapshot()
+})
+
+it('tokenizes parens', () => {
+  expect(Compiler.tokenizer('()')).toMatchSnapshot()
+})
+
+it('tokenizes names', () => {
+  expect(Compiler.tokenizer('foo')).toMatchSnapshot()
+})
+
+it('tokenizes a list', () => {
+  expect(Compiler.tokenizer('(123 foo (bar baz))')).toMatchSnapshot()
+})
